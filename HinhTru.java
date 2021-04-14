b380962ce0d7075b30de076ac66e8fb54698ddb3
phuchuho0402 <81024442+phuchuho0402@users.noreply.github.com>
2021-04-14 11:58:05 +0700

    Update HinhTru.java

--- HinhTru.java
+++ HinhTru.java
@@ -44,7 +44,7 @@ public class HinhTru extends JFrame {
 	//Thiet lap khoi tao GUI
 	public HinhTru() {
 		setTitle("Dien tich xung quanh hinh tru");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
